18fa60a2546f992fa570c6bfafca92a6b470c0fd
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-09-12 21:20:16 +0600

    feat(provider): introduce Create in service layer

--- service/providers.go
+++ service/providers.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
+
 	"github.com/im-sanny/service-finder/model"
 	"github.com/im-sanny/service-finder/repository"
 )
@@ -26,3 +30,22 @@ func NewProvider(repo repository.ProviderRepository, serviceRepo repository.Serv
 	}
 }
 
+func (s *provider) Create(p *model.Provider) error {
+	if p.Name == nil || *p.Name == "" {
+		return fmt.Errorf("service name required: %w", ErrInvalidInput)
+	}
+	if p.ServiceID != nil {
+		_, err := s.serviceRepo.GetById(*p.ServiceID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return fmt.Errorf("service id %d does not exists: %w", *p.ServiceID, ErrNotFound)
+			}
+			return fmt.Errorf("failed to verify service %d: %w", *p.ServiceID, err)
+		}
+	}
+
+	if err := s.repo.Create(p); err != nil {
+		return fmt.Errorf("failed to create provider: %w", err)
+	}
+	return nil
+}
